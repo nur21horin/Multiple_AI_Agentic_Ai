8ad2678a0fd6a46e55a6fc1eeafbe2188ae33729
Nur Mohammod <nurm98371@gmail.com>
2026-07-28 18:40:50 +0600

    Add'

--- chatmodels/chat.py
+++ chatmodels/chat.py
@@ -1,8 +1,22 @@
+
+
+# model = init_chat_model(
+#     "gemini-2.5-flash-lite",
+#     model_provider="google_genai"
+# )
+# #print(model)
+
+# response=model.invoke("What is cricketer ?")
+# print(response.content)
 from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
 
 load_dotenv()
 
-from langchain.chat_models import init_chat_model
+model = init_chat_model(
+    "gemini-3.6-flash",
+    model_provider="google_genai"
+)
 
-model=init_chat_model("gpt-4.1")
-#print(model)
+response = model.invoke("What is a cricketer?")
+print(response.content)
