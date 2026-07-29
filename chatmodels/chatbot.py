26fb70745e91e254bd9cfe7f23ef3f107885f6ae
Nur Mohammod <nurm98371@gmail.com>
2026-07-30 02:00:17 +0600

    Add

---
+++ chatmodels/chatbot.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_mistralai import ChatMistralAI
+
+model=ChatMistralAI(model="mistral-small-2506",temperature=0.9)
+
+response=model.invoke("Write a nur poiem ")
+
+print(response.content)
