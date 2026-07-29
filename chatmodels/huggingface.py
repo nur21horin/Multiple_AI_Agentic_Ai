da8565cf056212df9fb4a9a3a24f5f2798eb1e52
Nur Mohammod <nurm98371@gmail.com>
2026-07-29 23:42:29 +0600

    Add

--- chatmodels/huggingface.py
+++ chatmodels/huggingface.py
@@ -1,28 +1,28 @@
-# from dotenv import load_dotenv
-# from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
-
-# load_dotenv()
-
-# llm=HuggingFaceEndpoint(
-#     repo_id="deepseek-ai/DeepSeek-R1"
-# )
-
-# model=ChatHuggingFace(llm=llm)
-
-# response=model.invoke("Who are you ?")
-
-# print(response.content)
-
 from dotenv import load_dotenv
-from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
 
 load_dotenv()
 
-llm = HuggingFaceEndpoint(
-    repo_id="Qwen/Qwen2.5-7B-Instruct"
+llm=HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-R1"
 )
 
-chat = ChatHuggingFace(llm=llm)
+model=ChatHuggingFace(llm=llm)
 
-response = chat.invoke("Who are you?")
+response=model.invoke("Who are you ?")
+
 print(response.content)
+
+# from dotenv import load_dotenv
+# from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+
+# load_dotenv()
+
+# llm = HuggingFaceEndpoint(
+#     repo_id="Qwen/Qwen2.5-7B-Instruct"
+# )
+
+# chat = ChatHuggingFace(llm=llm)
+
+# response = chat.invoke("Who are you?")
+# print(response.content)
